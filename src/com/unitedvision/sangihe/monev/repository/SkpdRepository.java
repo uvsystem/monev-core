0423f901c7e2f730e1f0dd959f623b5661967c3a
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-06-15 23:15:22 +0800

    Fix skpd repo.

--- src/com/unitedvision/sangihe/monev/repository/SkpdRepository.java
+++ src/com/unitedvision/sangihe/monev/repository/SkpdRepository.java
@@ -3,11 +3,14 @@ package com.unitedvision.sangihe.monev.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.unitedvision.sangihe.monev.entity.Skpd;
 
 public interface SkpdRepository extends JpaRepository<Skpd, Integer> {
 
-	List<Skpd> findByKodeContainingOrNamaContaining(String keyword);
+	@Query(value = "Select s From Skpd s Where s.nama Like Concat('%', :keyword, '%') Or s.kode Like Concat('%', :keyword, '%')")
+	List<Skpd> findByKodeContainingOrNamaContaining(@Param("keyword") String keyword);
 
 }
